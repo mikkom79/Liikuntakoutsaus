46c775acec1d7acb4fcd3015ad20875067414ef6
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-04-13 09:00:38 +0300

    placeholder content

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,4 +1,32 @@
 function App() {
+  const questions = [
+    {
+      questionTitle: "Elit sint tempor ex adipisicing non reprehenderit.",
+      answerOptions: [
+        { optionText: "Cillum veniam et nostrud est amet.", addPoint: false },
+        { optionText: "Nostrud aute dolor ut eu voluptate sit eu ea mollit velit", addPoint: true },
+        { optionText: "Adipisicing commodo eiusmod velit cupidatat adipisicing quis", addPoint: true },
+      ],
+    },
+    {
+      questionTitle: "Elit sint tempor ex adipisicing non reprehenderit.",
+      answerOptions: [
+        { optionText: "Cillum veniam et nostrud est amet.", addPoint: false },
+        { optionText: "Nostrud aute dolor ut eu voluptate sit eu ea mollit velit", addPoint: true },
+        { optionText: "Adipisicing commodo eiusmod velit cupidatat adipisicing quis", addPoint: false },
+      ],
+    },
+    {
+      questionTitle: "Elit sint tempor ex adipisicing non reprehenderit.",
+      answerOptions: [
+        { optionText: "Cillum veniam et nostrud est amet.", addPoint: true },
+        { optionText: "Nostrud aute dolor ut eu voluptate sit eu ea mollit velit", addPoint: false },
+        { optionText: "Adipisicing commodo eiusmod velit cupidatat adipisicing quis", addPoint: true },
+      ],
+    },
+
+  ];
+
   return (
     <div>
       <h1>Liikuntakoutsaus</h1>
