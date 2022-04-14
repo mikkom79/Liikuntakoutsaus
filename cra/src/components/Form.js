4de310e3e0d41fa81dfd5094e234a92dde2b8b7e
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-04-14 15:42:20 +0300

    summary, small fixes

--- cra/src/components/Form.js
+++ cra/src/components/Form.js
@@ -3,8 +3,9 @@ import Section from "./Section";
 import Summary from "./Summary";
 
 const Form = () => {
-
-  const [answer, setAnswer] = useState([]);
+  const [answer1, setAnswer1] = useState();
+  const [answer2, setAnswer2] = useState();
+  const [answer3, setAnswer3] = useState();
 
   const initialState = { step: 1 };
 
@@ -24,7 +25,6 @@ const Form = () => {
   const questions = [
     {
       questionTitle: "QuestionTitleA",
-      id:1,
       answerOptions: [
         { id: 1, optionText: "Option A1", addPoint: false },
         { id: 2, optionText: "Option A2", addPoint: true },
@@ -33,7 +33,6 @@ const Form = () => {
     },
     {
       questionTitle: "QuestionTitleB",
-      id:2,
       answerOptions: [
         { id: 1, optionText: "Option B1", addPoint: false },
         { id: 2, optionText: "Option B2", addPoint: true },
@@ -43,7 +42,6 @@ const Form = () => {
     },
     {
       questionTitle: "QuestionTitleC",
-      id:3,
       answerOptions: [
         { id: 1, optionText: "Option C1", addPoint: false },
         { id: 2, optionText: "Option C2", addPoint: true },
@@ -56,14 +54,25 @@ const Form = () => {
     e.preventDefault();
   };
 
-  const handleChange = (e, text) => {
-    setAnswer([{q: e.target.name, a: e.target.value}])
-    console.log(answer);
+  const handleChange = (e) => {
+    switch (e.target.name) {
+      case "question1":
+        setAnswer1(e.target.value);
+        break;
+      case "question2":
+        setAnswer2(e.target.value);
+        break;
+      case "question3":
+        setAnswer3(e.target.value);
+        break;
+      default:
+        return;
+    }
   };
 
   return (
     <form className="quiz-form" onSubmit={handleSubmit}>
-      {state.step && (
+      {state.step !== questions.length+1 && (
         <Section
           questions={questions}
           step={state.step}
@@ -71,7 +80,7 @@ const Form = () => {
         />
       )}
 
-      {state.step !== questions.length && (
+      {state.step !== questions.length+1 && (
         <button id="next" onClick={() => dispatch({ type: "next" })}>
           Seuraava
         </button>
@@ -82,7 +91,15 @@ const Form = () => {
           Edellinen
         </button>
       )}
-      <Summary answers={answer}/>
+
+      {state.step === questions.length+1 &&
+      <Summary
+      questions={questions}
+      answer1={answer1}
+      answer2={answer2}
+      answer3={answer3}
+      />}
+     
     </form>
   );
 };
--- cra/src/components/Summary.js
+++ cra/src/components/Summary.js
@@ -1,13 +1,33 @@
-const Summary = ({answers}) => {
-    return ( 
-        <ul>
-            {answers.map((answer) => (
-            <li key={answer.q}>
-            <p>Question: {answer.q}</p>
-            <p>Answer: {answer.a}</p>
-            </li>))}
-        </ul>
-     );
-}
- 
+const Summary = ({ questions, answer1, answer2, answer3 }) => {
+  const questionsArr = questions.map((question) => question.questionTitle);
+  const TEXT_BEFORE = "Vastaus: ";
+  const TEXT_ALT = "Ei vastausta";
+
+  return (
+    <ul className="summary">
+      <li>
+        <p>{questionsArr[0]}</p>
+        <p>
+          {TEXT_BEFORE}
+          {answer1 || TEXT_ALT}
+        </p>
+      </li>
+      <li>
+        <p>{questionsArr[1]}</p>
+        <p>
+          {TEXT_BEFORE}
+          {answer2 || TEXT_ALT}
+        </p>
+      </li>
+      <li>
+        <p>{questionsArr[2]}</p>
+        <p>
+          {TEXT_BEFORE}
+          {answer3 || TEXT_ALT}
+        </p>
+      </li>
+    </ul>
+  );
+};
+
 export default Summary;
