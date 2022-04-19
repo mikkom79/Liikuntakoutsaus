8023a422b50ecaaa66f708966d6969ad09a9ffbf
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-04-19 14:39:05 +0300

    setting up PreQuiz.js

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import Form from "./components/Form";
+import PreQuiz from "./components/PreQuiz";
 
 function App() {
-  const [preDone, setPreDone] = useState(true);
+
+   const [preDone, setPreDone] = useState(false);
+   const [quizDone, setQuizDone] = useState(false);
+   const [quizResult, setQuizResult] = useState(undefined);
+
  //tarkista, onko alkukartoitus (työkyky 0-10) tehty
   return (
     <div className="App">
+      {!preDone && <PreQuiz setPreDone={setPreDone} />}
       {preDone && <Form />} 
     </div>
   );
---
+++ cra/src/components/PreQuiz.js
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+const PreQuiz = ({ setPreDone }) => {
+  const [showForm, setShowForm] = useState(false);
+  const [showResult, setShowResult] = useState(false);
+
+  const handleSubmit = (e) => {
+        e.preventDefault();
+        setPreDone(true);
+    }
+
+  return (
+    <div>
+      {!showForm && (
+        <p>
+          Et in nisi commodo esse non dolore adipisicing qui aute culpa irure
+          cupidatat. Qui pariatur reprehenderit est nostrud officia esse tempor
+          dolore occaecat ad id elit.
+          <br />
+          Consequat consectetur aliquip enim nulla.
+        </p>
+      )}
+      {!showForm && (
+        <button onClick={() => setShowForm(true)}>Tee testi</button>
+      )}
+
+      {showForm && (
+        <form onSubmit={handleSubmit}>
+          <p>
+            Velit elit culpa ea nisi aliquip consectetur commodo sit quis
+            laborum magna Lorem. Deserunt laboris aliquip eu culpa. Mollit
+            excepteur ex ex occaecat aliqua in Lorem excepteur?
+          </p>
+        </form>
+      )}
+      {showForm && <button type="submit" onClick={handleSubmit}>Lähetä vastaus</button>}
+    </div>
+  );
+};
+
+export default PreQuiz;
