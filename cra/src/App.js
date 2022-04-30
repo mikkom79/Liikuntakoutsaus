a129dfcc7dce112235016623604db443b9a7ce82
KaroliinaKoo <89521784+KaroliinaKoo@users.noreply.github.com>
2022-04-30 13:35:16 +0300

    removed react-responsive-modal

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import { Modal } from "react-responsive-modal";
-import "react-responsive-modal/styles.css";
 import Quiz from "./components/Quiz";
 import PreQuiz from "./components/PreQuiz";
 import Result from "./components/Result";
@@ -14,10 +12,7 @@ function App() {
   // onko pääkysely tehty
   const [recommendCoaching, setRecommendCoaching] = useState(undefined);
   // kyselyn tulos (pistekertymän mukaan)
-  const [showErrorModal, setShowErrorModal] = useState(false);
-  // virheilmoitus, jos käyttäjä yrittää lähettää puutteellisen kyselyn
-  const onCloseModal = () => setShowErrorModal(false);
-  // sulkee virheilmoituksen
+
   const reset = () => {
     setQuizDone(false);
     setPreDone(false);
@@ -30,18 +25,11 @@ function App() {
         <Quiz
           setQuizDone={setQuizDone}
           setRecommendCoaching={setRecommendCoaching}
-          setShowErrorModal={setShowErrorModal}
         />
       )}
       {quizDone && (
         <Result recommendCoaching={recommendCoaching} reset={reset} />
       )}
-
-      <Modal open={showErrorModal} onClose={onCloseModal} center>
-        <p className="modal" data-icon={alertIcon}>
-          Vastaa kaikkiin kysymyksiin.
-        </p>
-      </Modal>
     </div>
   );
 }
